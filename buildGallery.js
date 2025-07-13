1e9753e9784d7afe09cd52d8125087ab14e88bc9
Nicola Page <nicolapage@protonmail.com>
2025-07-13 16:49:05 +0100

    initial sketches uses buildGallery.js

---
+++ buildGallery.js
@@ -0,0 +1,24 @@
+function Build(directory){
+    const repo = "nickyp916/Barely-Fine-Art";
+    const folderPath = "images/dnd/";
+    const gallery = document.getElementById("gallery");
+
+    fetch(`https://api.github.com/repos/${repo}/contents/${folderPath}${directory}`)
+    .then(response => response.json())
+    .then(files => {
+        files.forEach(file => {
+        if (file.type === "file" && /\.(jpg|jpeg|png|gif)$/i.test(file.name)) {
+            const img = document.createElement("img");
+            img.src = file.download_url;
+            img.alt = file.name;
+
+            const container = document.createElement("div");
+            container.className = "image-item";
+            container.appendChild(img);
+
+            gallery.appendChild(container);
+        }
+        });
+    })
+    .catch(error => console.error("Error fetching images:", error));
+}       
