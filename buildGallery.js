48d51703404365980fef36806f40e6eaa8b88e0f
Nicola Page <nicolapage@protonmail.com>
2025-07-13 17:08:23 +0100

    all pages using script to build gallery

--- buildGallery.js
+++ buildGallery.js
@@ -1,4 +1,4 @@
-function Build(directory){
+function Build(directory, inclueCaptions){
     const repo = "nickyp916/Barely-Fine-Art";
     const folderPath = "images/dnd/";
     const gallery = document.getElementById("gallery");
@@ -7,14 +7,21 @@ function Build(directory){
     .then(response => response.json())
     .then(files => {
         files.forEach(file => {
-        if (file.type === "file" && /\.(jpg|jpeg|png|gif)$/i.test(file.name)) {
+        if (file.type === "file" && /\.(jpg|jpeg|png)$/i.test(file.name)) {
             const img = document.createElement("img");
+            const fileName = file.name.replace(/\.[^/.]+$/, "");
             img.src = file.download_url;
-            img.alt = file.name;
+            img.alt = fileName;
 
             const container = document.createElement("div");
             container.className = "image-item";
             container.appendChild(img);
+
+            if(inclueCaptions){
+                const caption = document.createElement("p");
+                caption.textContent = fileName;
+                container.appendChild(caption);
+            }
 
             gallery.appendChild(container);
         }
